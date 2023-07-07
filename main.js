734e4c3a26c6928d9e6ef2f7e4f59aae62a818a9
unknown <abumohammadnayemkhan@gmail.com>
2023-07-08 01:09:40 +0600

    learning javascript

---
+++ main.js
@@ -0,0 +1,56 @@
+//==========For loop start
+
+// for(let i = 0; i < 10; i = i + 1){
+//     if( i == 5 ){
+//         continue;
+//     }
+//     document.write("I'm" + i + "<br>");
+// };
+
+
+// breack a loop in 4th position 
+
+// for (let i = 0; i < 10; i++){
+//     if(i == 7){
+//         break;
+//     }
+//     console.log("I'm" + i);
+// }
+
+//==========For loop ended
+
+//==========While loop  start
+// let i = 0;
+// while(i<3){
+//     console.log("While - " + i);
+//     i = i + 1;
+// };
+//==========While loop ended
+
+//==========Do while loop start
+// let i = 0;
+// do{
+//     console.log("Do while" + i);
+//     i = i + 1;
+// }while(i < 10);
+//==========Do while loop ended
+
+//==========Javascript function start
+// function addTwoNumber(){
+//     let x = 10;
+//     let y = 11;
+//     let z = x + y;
+//     console.log(z);
+// }
+// addTwoNumber();
+//==========javascript function ended
+
+//==========Javascript function paramitter start
+// function paraPass(paraOne, ParaTwo){
+//     let x = Number(paraOne);
+//     let y = Number(ParaTwo);
+//     let sum = x + y;
+//     console.log(sum);
+// }
+// console.log(10, 20);
+//==========javascript function paramitter ended
